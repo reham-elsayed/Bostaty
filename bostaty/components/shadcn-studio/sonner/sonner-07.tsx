ad4bd74cff44ca4bdfbb2a4b7620014f7478580c
reham-elsayed <rehamshipl445@gmail.com>
2026-02-18 21:21:58 +0200

    typescript fix

--- bostaty/components/shadcn-studio/sonner/sonner-07.tsx
+++ bostaty/components/shadcn-studio/sonner/sonner-07.tsx
@@ -9,9 +9,17 @@ const SonnerWithPromiseDemo = () => {
   return (
     <Button
       variant='outline'
-      onClick={
+      onClick={() => {
+        const promise = new Promise((resolve) => setTimeout(() => resolve({ name: 'Sonner' }), 2000));
 
-      }
+        toast.promise(promise, {
+          loading: 'Loading...',
+          success: (data) => {
+            return `${(data as any).name} toast has been added`;
+          },
+          error: 'Error',
+        });
+      }}
     >
       Toast with promise
     </Button>
